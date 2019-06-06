193eec763b0f9f97f0ff7617bdd29f19350942cf
enockyang <enock0504@utexas.edu>
2019-06-06 14:08:49 -0500

    lab2 update c+d 2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -52,7 +52,7 @@ public class IntList {
             return null;
         }
         IntList res = new IntList(L.first * L.first, null);
-        IntList ptr = res;
+        IntList ptr = res; // ptr --> temp
         L = L.rest;
         while (L != null) {
             ptr.rest = new IntList(L.first * L.first, null);
@@ -82,44 +82,71 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if (B == null){
-            return A;
+
+        /* iteration */
+        if (A == null){
+            return B;
+            //解决空集问题???
+        } else{
+            IntList temp = A;
+            while (temp.rest!= null){
+                temp = temp.rest;
+            }
+            temp.rest = B;
         }
-        IntList p = A;
-        while (p.rest != null){
-            p = p.rest;
-        }
-        p.rest = B;
         return A;
+
+//        /* recursion */
+//        if (A == null){
+//            return B;
+//        }
+//        else {
+//            A.rest = dcatenate (A.rest, B);
+//            return A;
+//        }
+
+
+
     }
 
     /**
      * Returns a list consisting of the elements of A followed by the
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
+    // however the IntList returned by IntList.catenate(A, B) has a pointer to B.
     public static IntList catenate(IntList A, IntList B) {
-        IntList p = new IntList();
-        IntList q = p;
-        IntList a = A;
-        IntList b = B;
-        while (a.rest!= null){
-            q.first = a.first;
-            q.rest = new IntList();
-            a = a.rest;
-            q = q.rest;
+        /* recursion */
+        if (A == null){
+            return B;
         }
-        q.first = a.first;
-        q.rest = new IntList();
-        q = q.rest;
+        else {
+            return new IntList(A.first, catenate(A.rest, B));
+        }
 
-        while (b.rest!= null){
-            q.first = b.first;
-            q.rest = new IntList();
-            b = b.rest;
-            q = q.rest;
-        }
-        q.first = b.first;
-        return p;
+//        /* iteration */
+//        IntList p = new IntList();
+//        IntList q = p;
+//        IntList a = A;
+//        IntList b = B;
+//        while (a.rest!= null){
+//            q.first = a.first;
+//            q.rest = new IntList();
+//            a = a.rest;
+//            q = q.rest;
+//        }
+//        q.first = a.first;
+//        q.rest = new IntList();
+//        q = q.rest;
+//
+//        while (b.rest!= null){
+//            q.first = b.first;
+//            q.rest = new IntList();
+//            b = b.rest;
+//            q = q.rest;
+//        }
+//        q.first = b.first;
+//        return p;
+
     }
 
 
--- lab2/Intlist/IntListTest.java
+++ lab2/Intlist/IntListTest.java
@@ -62,11 +62,23 @@ public class IntListTest {
 
     @Test
     public void testDcatenate() {
-        IntList A = IntList.of(1, 2, 3);
+        IntList A = IntList.of(1, 2, 3); // A is a class, a reference type, so it is passed by address
         IntList B = IntList.of(4, 5, 6);
         IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
         assertEquals(exp, IntList.dcatenate(A, B));
         assertEquals(IntList.of(1, 2, 3, 4, 5, 6), A);
+        /* Q1 */
+        // however the IntList returned by IntList.catenate(A, B) has a pointer to B.
+        // which means if we change B then the new IntList will also change. Is this correct?
+
+//        IntList A2 = IntList.of(); // A2 is null, a primitive data type, so it is passed by value
+//        IntList B2 = IntList.of(4, 5, 6);
+//        assertEquals(IntList.of(4, 5, 6),IntList.dcatenate(A2, B2));
+//        assertEquals(IntList.of(4, 5, 6),A2); // Always wrong --> A2 will be null forever ??? How to solve that?
+//        /* Q2 */
+//        // A2 is null, a primitive data type, so it is passed by value
+//        // Always wrong <-- A2 will be null forever ??? How to solve that?
+
     }
 
     @Test
@@ -76,6 +88,7 @@ public class IntListTest {
         IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
         assertEquals(exp, IntList.catenate(A, B));
         assertEquals(IntList.of(1, 2, 3), A);
+
     }
 
 }

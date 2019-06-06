d01e47907427b2267500fad5af3fec55c0575d62
enockyang <enock0504@utexas.edu>
2019-06-06 12:04:03 -0500

    lab1 update2

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,8 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
+        int sumx = 0;
         while (x < 10) {
-            System.out.print(x + " ");
+            sumx += x;
+            System.out.print( sumx + " ");
             x = x + 1;
         }
     }
